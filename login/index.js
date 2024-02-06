f1a7617697b420e93d2c24f320a72ae9a23fbb23
Richard Henrique <richardhs826@gmail.com>
2024-02-06 20:49:25 -0300

    LoginPage

---
+++ login/index.js
@@ -0,0 +1,93 @@
+
+import { firebaseConfig } from "../environment/environments.js";
+
+
+console.log(firebaseConfig)
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth()
+const database = firebase.database()
+
+
+var btn = document.getElementById('btn')
+
+btn.addEventListener('click', function () {
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email ou senha fora de formato!!')
+      return
+
+    }
+  
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+    
+      var user = auth.currentUser
+  
+   
+      var database_ref = database.ref()
+  
+  
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      database_ref.child('users/' + user.uid).update(user_data)
+  
+      alert('Logando...!!')
+      location.href = "../home.html"
+  
+    })
+    .catch(function(error) {
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+)
+
+
+function validate_email(email) {
+    var filter = /^[^@]+@\w+(\.\w+)+\w$/
+    if (filter.test(email)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validate_password(password) {
+    // Senha deve ser maior que 6
+    if (password < 6) {
+        return false
+    } else {
+        return true
+    }
+}
+function validate_user(usuario) {
+    // Senha deve ser maior que 6
+    if (usuario < 6) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function validate_field(field) {
+    if (field == null) {
+        return false
+    }
+
+    if (field.length <= 0) {
+        return false
+    } else {
+        return true
+    }
+}
+function letraNumSimb(str){
+    return /[a-zA-Z]/.test(str) && /[0-9]/.test(str) && /[^a-zA-Z0-9]/.test(str)
+}
--- login/loginsrc.js
+++ login/loginsrc.js
@@ -1,48 +1,15 @@
 
 function mostrar(){
-    let senha = document.querySelector(".senha")
+    let senha = document.querySelector("#password")
     let eye = document.querySelector(".olho")
     
     if(senha.type === "password"){
         senha.type = "text"
-        eye.src = "./imges/eye-open.png"
+        eye.src = "../imges/eye-open.png"
     } else {
         senha.type = 'password'
-        eye.src = "./imges/eye-close.png"
+        eye.src = "../imges/eye-close.png"
     }
 }
 
-
-
-function logar(){
-
-    let pegaUsuario = document.querySelector('.usuario').value
-    let pegaSenha = document.querySelector('.senha').value
-
-    let listaUsuario =  []
-    let userValid= {
-        login: '',
-        pass: ''
-    }
-    listaUsuario = JSON.parse(localStorage.getItem('listaUsuario'))
-    
-    listaUsuario.forEach((item) => {
-        if(pegaUsuario == item.login && pegaSenha == item.pass){
-            userValid = {
-                login: item.login,
-                pass: item.pass
-            }
-        }
-    })
-
-    if(pegaUsuario == userValid.login && pegaSenha == userValid.pass){
-        location.href = 'home.html'
-
-    }else{
-        let res= document.querySelector('.resposta')
-        res.style.color = 'red' 
-        res.innerHTML = 'Usuario ou senha incorreto(s).'
-    }
-
-    }
 
